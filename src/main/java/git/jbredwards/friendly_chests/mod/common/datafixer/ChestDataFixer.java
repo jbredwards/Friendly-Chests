618f012af8d97e30ddafd0b3e0162c96284acea6
jbredwards <47703524+jbredwards@users.noreply.github.com>
2025-11-19 18:46:03 -0500

    Fix a ConcurrentModificationException

--- src/main/java/git/jbredwards/friendly_chests/mod/common/datafixer/ChestDataFixer.java
+++ src/main/java/git/jbredwards/friendly_chests/mod/common/datafixer/ChestDataFixer.java
@@ -16,6 +16,8 @@ import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Map;
 
 /**
  * fix old chests on chunk load
@@ -26,37 +28,39 @@ import javax.annotation.Nonnull;
 public final class ChestDataFixer
 {
     @SubscribeEvent(priority = EventPriority.HIGH)
-    static void applyFix(@Nonnull ChunkEvent.Load event) {
-        final Chunk chunk = event.getChunk();
-        final World world = event.getWorld();
+    static void applyFix(@Nonnull final ChunkEvent.Load event) {
+        @Nonnull final Chunk chunk = event.getChunk();
+        @Nonnull final World world = event.getWorld();
 
-        chunk.getTileEntityMap().forEach((pos, tile) -> {
-            if(tile instanceof TileEntityChest) {
-                final IFriendlyChestCapability cap = IFriendlyChestCapability.get(tile);
-                if(cap != null && !cap.isFixed()) {
-                    IBlockState state = chunk.getBlockState(pos);
-                    if(state.getBlock() instanceof BlockChest) {
-                        //fix facing data
-                        EnumFacing facing = EnumFacing.byIndex(state.getBlock().getMetaFromState(state));
-                        if(facing.getAxis() == EnumFacing.Axis.Y) facing = EnumFacing.NORTH;
-                        state = state.withProperty(BlockChest.FACING, facing);
-                        //fix connected data
-                        ChestType type = ChestType.SINGLE;
-                        if(fixSide(state, world, chunk, pos, facing, ChestType.RIGHT)) type = ChestType.RIGHT;
-                        else if(fixSide(state, world, chunk, pos, facing, ChestType.LEFT)) type = ChestType.LEFT;
-                        state = state.withProperty(ChestType.TYPE, type);
-                        //set fixed state
-                        chunk.setBlockState(pos, state);
-                    }
+        //noinspection unchecked
+        for(@Nonnull final Map.Entry<BlockPos, TileEntityChest> entry : chunk.getTileEntityMap().entrySet().stream()
+        .filter(entry -> entry.getValue() instanceof TileEntityChest && !entry.getValue().isInvalid()).toArray(Map.Entry[]::new)) {
+            @Nonnull final BlockPos pos = entry.getKey();
+            @Nullable final IFriendlyChestCapability cap = IFriendlyChestCapability.get(entry.getValue());
 
-                    cap.setFixed(true);
+            if(cap != null && !cap.isFixed()) {
+                @Nonnull IBlockState state = chunk.getBlockState(pos);
+                if(state.getBlock() instanceof BlockChest) {
+                    //fix facing data
+                    @Nonnull EnumFacing facing = EnumFacing.byIndex(state.getBlock().getMetaFromState(state));
+                    if(facing.getAxis() == EnumFacing.Axis.Y) facing = EnumFacing.NORTH;
+                    state = state.withProperty(BlockChest.FACING, facing);
+                    //fix connected data
+                    @Nonnull ChestType type = ChestType.SINGLE;
+                    if(fixSide(state, world, chunk, pos, facing, ChestType.RIGHT)) type = ChestType.RIGHT;
+                    else if(fixSide(state, world, chunk, pos, facing, ChestType.LEFT)) type = ChestType.LEFT;
+                    state = state.withProperty(ChestType.TYPE, type);
+                    //set fixed state
+                    chunk.setBlockState(pos, state);
                 }
+
+                cap.setFixed(true);
             }
-        });
+        }
     }
 
     static boolean fixSide(@Nonnull IBlockState state, @Nonnull World world, @Nonnull Chunk chunk, @Nonnull BlockPos pos, @Nonnull EnumFacing facing, @Nonnull ChestType type) {
-        final BlockPos otherPos = pos.offset(type.rotate(facing));
+        final BlockPos otherPos = pos.offset(type.getSideAttached(facing));
         final Chunk otherChunk = chunk.isAtLocation(otherPos.getX() >> 4, otherPos.getZ() >> 4) ? chunk :
                 world.getChunkProvider().getLoadedChunk(otherPos.getX() >> 4, otherPos.getZ() >> 4);
 
